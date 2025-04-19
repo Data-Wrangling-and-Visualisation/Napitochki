6d28ae12e68bb1fdd4f319747ea1571b14a14a3a
Bulat Akhmatov <ahmatovbulat@gmail.com>
2025-04-19 20:54:29 +0300

    feat: dockerfile for frontend and vite config for remote server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,13 @@
 // vite.config.js
 export default {
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
-      '/drinks': {
-        target: 'http://89.169.174.146:8888', // Адрес твоего API
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/drinks/, '/drinks'), // Переписываем путь
-      },
       '/api': {
         target: 'http://89.169.174.146:8888', // API server address
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix when forwarding
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     },
   },
